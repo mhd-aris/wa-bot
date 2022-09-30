e065b7ca3c2578dc345103177dbf942aff11d925
Mhd Aris <mhdarisid@gmail.com>
2022-09-30 09:39:33 +0700

    add tiktok downloader

--- HandleMessages.js
+++ HandleMessages.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { insertLink, getLinks } from "./lib/postLink.js";
 
 const HandleMessages = async (client, message) => {
@@ -10,9 +11,32 @@ const HandleMessages = async (client, message) => {
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi
   );
 
+  console.log(command);
+
   switch (command) {
     case "/test":
       await client.sendText(from, " Hello " + message.notifyName + "!");
+      break;
+
+    case "/tiktok":
+      try {
+        let linkVt = args[1];
+        if (!linkVt.match(isUrl)) {
+          return await client.sendText(from, "Link not valid!");
+        }
+        let response = await axios.get(
+          `https://api.douyin.wtf/api?url=${linkVt}`
+        );
+        let urlVideo = await response.data.nwm_video_url;
+        await client.sendFileFromUrl(
+          from,
+          urlVideo,
+          "result.mp4",
+          `Download success! \n   by: @xryvq`
+        );
+      } catch (error) {
+        await client.sendText(from, "Failed to download video! :(");
+      }
       break;
     case "/link":
       let url = args[1];
@@ -26,7 +50,7 @@ const HandleMessages = async (client, message) => {
       }
       break;
 
-    case "/getLinks":
+    case "/getlinks":
       const response = await getLinks();
       if (!response.link) {
         const links = "";
